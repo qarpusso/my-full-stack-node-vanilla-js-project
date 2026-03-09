7dc90c8bb36bb6bc9d4454f54ddc4977bb081fcf
qarpusso <qarpusso@gmail.com>
2026-03-09 18:45:49 +0300

    checkpoint

---
+++ public/scripts/sign-up.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function(event){
+    const signUpFrom = document.getElementById("sign-up-form");
+    const usernameEl = document.getElementById("username");
+    const emailEl = document.getElementById("email");
+    const passwordEl = document.getElementById("password");
+    const confirmPasswordEl = document.getElementById("confirm-password");
+    const errorEl = document.getElementById("error");
+    signUpFrom.addEventListener("submit", async ()=>{
+        if(!usernameEl, !emailEl, !passwordEl, !confirmPasswordEl) return;
+        const username = usernameEl.value.trim();
+        const email = emailEl.value.trim();
+        const password = passwordEl.value.trim();
+        const confirmPassword = confirmPasswordEl.value.trim();
+        if(password !== confirmPassword){
+            errorEl.textContent = "passwords are not matching!"
+            return;
+        }
+    })
+});
